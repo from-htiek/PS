6bdd7b9084a458324ff7cd47e1eee80ad3437fc3
from-htiek <from.htiek@gmail.com>
2022-08-09 18:28:25 +0900

    220809

--- Baekjoon/src/G5_5430.java
+++ Baekjoon/src/G5_5430.java
@@ -7,9 +7,10 @@ import java.util.Iterator;
 import java.util.StringTokenizer;
 
 /*
- * 2022.08.07
+ * 2022.08.09
  * AC
- * 마의 33퍼.. 
+ * 구현보다는 입출력에서 걸리는게 많았던 문제
+ * deque로 구현했으나 가장 빠른건 비트연산인것같고 직접 빼는 작업을 하지 않고 인덱스로 관리하는것도 충분히 빠른듯하다.
  */
 public class G5_5430 {
 	public static void main(String[] args) throws IOException {
@@ -22,46 +23,16 @@ public class G5_5430 {
 		for(int tc = 0; tc < t; tc++) {
 			String p = br.readLine(); // 수행할 함수 
 			int n = Integer.parseInt(br.readLine()); // 배열의 개수
-			StringTokenizer st = new StringTokenizer(br.readLine(), ","); // 들어오는 배열 
-
+			String string = br.readLine().replace("[", "").replace("]", ""); // 들어오는 배열 
+			StringTokenizer st = new StringTokenizer(string, ","); 
 			
 			Deque<Integer> deque = new ArrayDeque<>(); // 숫자만 배열에 삽입
 			
-			if(n > 2) { // 입력, 하지만 이렇게 받으면 안될것같음 
-				int num = 0; 
-				String _num = st.nextToken();
-				if(_num.length() == 2) num += _num.charAt(1) - '0';
-				else if(_num.length() == 3) {
-					num += (_num.charAt(1) - '0') * 10;
-					num += _num.charAt(2) - '0';
-				}
-				deque.add(num);
-				
-				for(int i = 1; i < n-1; i++) {
-					deque.add(Integer.parseInt(st.nextToken()));
-				}
-				
-				
-				num = 0;
-				_num = st.nextToken();
-				if(_num.length() == 2) num += _num.charAt(0) - '0';
-				else if(_num.length() == 3) {
-					num += (_num.charAt(0) - '0') * 10;
-					num += _num.charAt(1) - '0';
-				}
-				deque.add(num);
-				
-			}else if(n == 1) {
-				int num = 0; 
-				String _num = st.nextToken();
-				if(_num.length() == 3) num += _num.charAt(1) - '0';
-				else if(_num.length() == 4) {
-					num += (_num.charAt(1) - '0') * 10;
-					num += _num.charAt(2) - '0';
-				}
-				deque.add(num);
+			for(int i = 0; i < n; i++) {
+				deque.add(Integer.parseInt(st.nextToken()));
 			}
-
+			
+			boolean flag = false; // 에러 체크
 			direction = 0; // 처음 디폴트 방향 0 
 			for(int i = 0, size = p.length(); i < size; i++) {
 				char ch = p.charAt(i);
@@ -69,7 +40,10 @@ public class G5_5430 {
 					if(direction == 0) direction = 1; // 방향 뒤집기
 					else direction = 0; 
 				}else { // D일 때 
-					if(deque.size() == 0) break; // 배열이 없으면 에러 
+					if(deque.size() == 0) {
+						flag = true;
+						break; // 배열이 없으면 에러 
+					}
 					
 					if(direction == 0) deque.removeFirst();
 					else deque.removeLast();
@@ -77,21 +51,24 @@ public class G5_5430 {
 				
 			}
 			
-			
-			if(!deque.isEmpty()) {
-				sb.append("[");
-				if(direction == 0) {
-					Iterator itr = deque.iterator();
-					while(itr.hasNext()) sb.append(itr.next()).append(",");
-				}else {
-					Iterator itr = deque.descendingIterator();
-					while(itr.hasNext()) sb.append(itr.next()).append(",");
-				}
-				
-				sb.deleteCharAt(sb.lastIndexOf(","));
-				sb.append("]\n");
-			}else {
+			if(flag) {
 				sb.append("error\n");
+			}else {
+				if(!deque.isEmpty()) {
+					sb.append("[");
+					if(direction == 0) {
+						Iterator itr = deque.iterator();
+						while(itr.hasNext()) sb.append(itr.next()).append(",");
+					}else {
+						Iterator itr = deque.descendingIterator();
+						while(itr.hasNext()) sb.append(itr.next()).append(",");
+					}
+					
+					sb.deleteCharAt(sb.lastIndexOf(","));
+					sb.append("]\n");
+				}else {
+					sb.append("[]\n");
+				}
 			}
 			
 			

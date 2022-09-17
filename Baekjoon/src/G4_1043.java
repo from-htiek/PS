0af545ca65f0da1a6d6d789110f518b936944414
from-htiek <from.htiek@gmail.com>
2022-09-18 01:39:58 +0900

    220917

--- Baekjoon/src/G4_1043.java
+++ Baekjoon/src/G4_1043.java
@@ -2,69 +2,72 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.Queue;
 import java.util.StringTokenizer;
-
 /*
- * 2022.09.16
+ * 2022.09.17
  * 거짓말
- * 예외
-10 10 
-1 1 
-2 10 1
-2 9 2
-2 8  3
-2 7 4
-2 6 5
-2 5 7 
-2 4 8
-2 3 9 
-2 2 10 
-1 1
  */
 public class G4_1043 {
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
-		int n = Integer.parseInt(st.nextToken()); // 사람의 수
-		int m = Integer.parseInt(st.nextToken()); // 파티의 수
+		int n = Integer.parseInt(st.nextToken()); // 사람 수
+		int m = Integer.parseInt(st.nextToken()); // 파티 수
 		
-		int[] truth = new int[n+1]; // 진실을 아는 사람 
+		
+		boolean[][] check = new boolean[n+1][n+1];
 		st = new StringTokenizer(br.readLine());
-		int t = Integer.parseInt(st.nextToken());
-		for(int tc = 0; tc < t; tc++) { 
-			truth[Integer.parseInt(st.nextToken())] = -1; 
+		Queue<Integer> q = new LinkedList<>(); // 진실을 알고 있는 사람 
+		int num = Integer.parseInt(st.nextToken());
+		for(int i = 0; i < num; i++) {
+			q.offer(Integer.parseInt(st.nextToken()));
 		}
 		
-		
-		for(int tc = 0; tc < m; tc++) { // 파티 수 만큼 반복 
-			boolean flag = false; // 진실을 아는 사람이 있는지 체크 
-			String str = br.readLine();
-			st = new StringTokenizer(str);
-			int num = Integer.parseInt(st.nextToken());
-			for(int i = 0; i < num; i++) {
-				if(truth[Integer.parseInt(st.nextToken())] == -1) { // 진실을 아는 사람이 있다 
-					flag = true; 
-					break; 
-				}
-			}
+		int[] party = new int[n+1]; // 파티에 몇 번 가는지 체크 하는 배열
+		for(int p = 0; p < m; p++) { // m번의 파티만큼 반복
+			st = new StringTokenizer(br.readLine());
+			num = Integer.parseInt(st.nextToken());
 			
-			st = new StringTokenizer(str);
-			st.nextToken();
-			if(flag) { // 있을 때는 같이 파티에 온 사람들도 모두 진실을 알고 있음 
-				for(int i = 0; i < num; i++) {
-					truth[Integer.parseInt(st.nextToken())] = -1;
-				}
-			}else { // 거짓말 가능, 일행 한명만 체크 
-				truth[Integer.parseInt(st.nextToken())]++; 
+			if(num == 1) {
+				party[Integer.parseInt(st.nextToken())]++; //
+			}else {
+				int index = Integer.parseInt(st.nextToken());
+				party[index]++;
+				while(st.hasMoreTokens()) { // 같이 온 사람 체크
+					check[index][Integer.parseInt(st.nextToken())] = true; 
+				}	
 			}
 		}
 		
-		int result = 0; 
-		for(int i = 1; i <= n; i++) {
-			if(truth[i] != -1) result += truth[i];
+//		for(boolean[] c : check) {
+//			System.out.println(Arrays.toString(c));
+//		}
+		
+		while(!q.isEmpty()) { // 진실을 알고 있는 사람 체크
+			int cur = q.poll();
+			party[cur] = -1;
+			
+			for(int i = 1; i <= n; i++) {
+				if(check[i][cur] || check[cur][i]) { // 같이 파티에 온 적 있다면 체크 
+					if(party[i] == -1) continue;
+					party[i] = -1;
+					q.offer(i);
+				}
+			
+			}
 		}
 		
-		System.out.println(Arrays.toString(truth));
+//		System.out.println(Arrays.toString(party));
+		
+		
+		int result = 0;
+		for(int i = 1; i <= n; i++) {
+			if(party[i] != -1) result += party[i];
+		}
+		
 		System.out.println(result);
+		
 	}
 }

08bd5fbf66d60dc478744161f47c89d06c8c38fa
from-htiek <from.htiek@gmail.com>
2022-11-04 22:47:13 +0900

    221103

--- Softeer/src/수퍼바이러스.java
+++ Softeer/src/수퍼바이러스.java
@@ -4,15 +4,18 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 /*
  * 2022.11.03
- * 왜 안되는거지? 
+ * 수퍼바이러스
+ * long의 최대값이 10^19라고한다.
+ * num * num * p의 순간 최대값은 10억 * 10억 * 1억.... 
+ *  (num * num % 1000000007)이렇게 고쳐줬더니  드디어 풀림
  */
 public class 수퍼바이러스 {
 	public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-        long k = Integer.parseInt(st.nextToken());
-        long p = Integer.parseInt(st.nextToken());
-        long n = Integer.parseInt(st.nextToken());
+        long k = Long.parseLong(st.nextToken());
+        long p = Long.parseLong(st.nextToken());
+        long n = Long.parseLong(st.nextToken());
         
         long result = func(p, n*10);
         System.out.println(k * result % 1000000007);
@@ -26,7 +29,7 @@ public class 수퍼바이러스 {
 			return num * num % 1000000007;
 		}else {
 			long num = func(p, n/2);
-			return num * num * p % 1000000007;
+			return (num * num % 1000000007) * p % 1000000007;
 		}
 	}
 }

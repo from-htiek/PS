9310f6c117d6a2fd717f70cd835e0e9a97129052
from-htiek <from.htiek@gmail.com>
2022-11-17 23:01:01 +0900

    221117

---
+++ Baekjoon/src/B4_16486.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+/*
+ * 2022.11.17
+ * 운동장 한 바퀴
+ */
+public class B4_16486 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int d1 = sc.nextInt();
+		int d2 = sc.nextInt();
+		
+		System.out.println(d1*2 + d2*2*3.141592);
+	}
+}

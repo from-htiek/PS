ecdf6517f0514967e48ca2b10a4e27a5add2079a
from-htiek <from.htiek@gmail.com>
2022-11-27 10:10:01 +0900

    221127

---
+++ Baekjoon/src/B2_1453.java
@@ -0,0 +1,29 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+/*
+ * 2022.11.27
+ * 피시방 알바
+ */
+public class B2_1453 {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int n = Integer.parseInt(br.readLine());
+		
+		boolean[] seat = new boolean[101];
+		int result = 0; 
+		
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		for(int i = 0; i < n; i++) {
+			int num = Integer.parseInt(st.nextToken());
+			if(seat[num]) result++;
+			else seat[num] = true;
+		}
+		
+		System.out.println(result);
+		
+	}
+}

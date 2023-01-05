2d4998227f5219abf209627aafdb6b4711ef29a9
from-htiek <from.htiek@gmail.com>
2023-01-05 23:38:08 +0900

    230105

--- Baekjoon/src/G4_14226.java
+++ Baekjoon/src/G4_14226.java
@@ -1,70 +1,67 @@
+import java.util.LinkedList;
 import java.util.PriorityQueue;
+import java.util.Queue;
 import java.util.Scanner;
 
 /*
- * 2022.12.19
+ * 2022.01.05
  * 이모티콘
- * 문제 이해를 잘못한것 같은데..?
+ * 꼭 방문 배열을 1002로 줘야할까 내가 짜놓고도 근거가 부족한것같은데
  */
 public class G4_14226 {
-	public static class number implements Comparable<number>{
-		int num;
+	public static class emoji {
+		int screen;
+		int clipboard;
 		int cnt;
 		
-		public number(int num, int cnt) {
+		public emoji(int screen, int clipboard, int cnt) {
 			super();
-			this.num = num;
+			this.screen = screen;
+			this.clipboard = clipboard;
 			this.cnt = cnt;
 		}
 
 		@Override
-		public int compareTo(number o) {
-			// TODO Auto-generated method stub
-			return Integer.compare(this.cnt, o.cnt);
-		}
-
-		@Override
 		public String toString() {
-			return "number [num=" + num + ", cnt=" + cnt + "]";
-		} 
-		
+			return "emoji [screen=" + screen + ", clipboard=" + clipboard + ", cnt=" + cnt + "]";
+		}		
 	}
+	
 	public static void main(String[] args) {
 		Scanner scanner = new Scanner(System.in);
 		int s = scanner.nextInt();
 		
-		PriorityQueue<number> pq = new PriorityQueue<>();
-		pq.offer(new number(1, 0));
+		Queue<emoji> pq = new LinkedList<>();
+		boolean[][] visitied = new boolean[1002][1002];
+		pq.offer(new emoji(1, 0, 0));
 
 		while(!pq.isEmpty()) {
-			number cur = pq.poll();
+			emoji cur = pq.poll();
 			
-			if(cur.num == s) {
+			if(cur.screen == s) {
 				System.out.println(cur.cnt);
 				return;
 			}
 			
 			for(int d = 0; d < 3; d++) {
-				int news = -1; 
-				int newcnt = -1;
-				if(d == 0) {
-					news = cur.num * 2;
-					newcnt = cur.cnt + 2; 
-				}
-				else if(d == 1) {
-					news = cur.num - 1;
-					newcnt = cur.cnt + 1;
-				}
-				else {
-					if(s % cur.num == 0) {
-						news = s; 
-						newcnt = cur.cnt + s / cur.num;
-					}
+				int newscreen = cur.screen;
+				int newclip = cur.clipboard;
+				
+				if(d == 0) { // 1. 화면에 있는 이모티콘을 모두 복사해서 클립보드에 저장
+					newclip = cur.screen;
+				}else if(d == 1) { // 2. 클립보드에 있는 모든 이모티콘을 화면에 붙여넣기
+					newscreen = cur.screen + cur.clipboard;
+				}else { // 3. 화면에 있는 이모티콘 중 하나를 삭제
+					newscreen = cur.screen - 1;
+					
+					if(newscreen <= 0) continue;
 				}
 				
-				if(news < 1 || news > 1002) continue;
-				pq.offer(new number(news, newcnt));
+				if(newscreen < 0 || newscreen > 1001 || newclip < 0 || newclip > 1001) continue;
+				if(visitied[newscreen][newclip]) continue;
 				
+				pq.offer(new emoji(newscreen, newclip, cur.cnt+1));
+				visitied[newscreen][newclip] = true;
 			}
 		
 		}

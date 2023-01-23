82b573339ce1a9d931bf56f8f9e839ee5d044972
from-htiek <from.htiek@gmail.com>
2023-01-23 22:26:16 +0900

    230123

--- Baekjoon/src/S3_19637.java
+++ Baekjoon/src/S3_19637.java
@@ -6,15 +6,18 @@ import java.util.HashMap;
 import java.util.StringTokenizer;
 
 /*
- * 2023.01.17
+ * 2023.01.23
  * IF문 좀 대신 써줘
- * 내일 범위 바꿔서 다시 풀어보는걸로
+ * 1. Stringbuilder의 힘은 생각보다 강력함
+ * 2. 아직 나는 이분탐색을 잘 모름
+ * return을 list[start]로 해줘야하는구나.. 여기서 한참 헤맸다
  */
 public class S3_19637 {
 	public static int list[], cnt;
 	public static HashMap<Integer, String> hashmap;
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
 		StringTokenizer st = new StringTokenizer(br.readLine());
 		int n = Integer.parseInt(st.nextToken()); // 칭호 개수
 		int m = Integer.parseInt(st.nextToken()); // 캐릭터 개수
@@ -22,7 +25,7 @@ public class S3_19637 {
 		hashmap = new HashMap<>();
 		list = new int[n];
 		Arrays.fill(list, 1000000001);
-		cnt = 0;
+		cnt = 0; // 중복을 제외한 칭호 개수
 		
 		for(int i = 0; i < n; i++) {
 			st = new StringTokenizer(br.readLine());
@@ -36,24 +39,24 @@ public class S3_19637 {
 			}
 		}
 		
-		Arrays.sort(list);
-		System.out.println(hashmap);
-//		System.out.println(Arrays.toString(list));
+		Arrays.sort(list); // 정렬
 		
 		for(int i = 0; i < m; i++) {
-			System.out.println(binarySearch(0, cnt-1, Integer.parseInt(br.readLine()), 0));
+			sb.append((hashmap.get(binarySearch(0, cnt-1, Integer.parseInt(br.readLine()))))).append("\n");
 		}
 		
+		System.out.println(sb.toString());
 	}
 	
-	public static int binarySearch(int start, int end, int key, int idx) {
-		if(start > end || end < 0 || start >= cnt) return idx;
+	// 이분탐색 
+	public static int binarySearch(int start, int end, int key) {
+		if(start > end ) return list[start]; 
 		else {
 			int mid = (start + end) / 2;
 			
-			if(list[mid] == key) return mid;
-			else if(list[mid] < key) return binarySearch(mid+1, end, key, mid+1);
-			else return binarySearch(start, mid-1, key, mid-1);
+			if(list[mid] == key) return list[mid];
+			else if(list[mid] < key) return binarySearch(mid+1, end, key);
+			else return binarySearch(start, mid-1, key);
 		}
 	}
 }

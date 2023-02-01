9cc9951e037df7bf30de1f57e1572f0e2a0c88fd
from-htiek <from.htiek@gmail.com>
2023-02-01 23:05:54 +0900

    230201

---
+++ Baekjoon/src/B5_5338.java
@@ -0,0 +1,15 @@
+import java.io.IOException;
+
+/*
+ * 2023.02.01
+ * 마이크로소프트 로고
+ */
+public class B5_5338 {
+	public static void main(String[] args) throws IOException {
+        System.out.print("       _.-;;-._\n" +
+                "'-..-'|   ||   |\n" +
+                "'-..-'|_.-;;-._|\n" +
+                "'-..-'|   ||   |\n" +
+                "'-..-'|_.-''-._|");
+	}
+}

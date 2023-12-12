50f2e91eebe04df35c746de1364dea74388fcf21
from-htiek <from.htiek@gmail.com>
2023-12-12 21:46:30 +0900

    231212

---
+++ Baekjoon/src/B5_10171.java
@@ -0,0 +1,12 @@
+/*
+ * 2023.12.12
+ * 고양이
+ */
+public class B5_10171 {
+	public static void main(String[] args) {
+		System.out.println("\\    /\\");
+		System.out.println(" )  ( ')");
+		System.out.println("(  /  )");
+		System.out.println(" \\(__)|");
+	}
+}

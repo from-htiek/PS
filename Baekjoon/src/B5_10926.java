b087987fc54c95a37fea1ce444bee86e77822fd3
from-htiek <from.htiek@gmail.com>
2023-12-13 22:59:59 +0900

    231213

---
+++ Baekjoon/src/B5_10926.java
@@ -0,0 +1,19 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+/*
+ * 2023.12.13
+ * ??!
+ */
+public class B5_10926 {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		
+		sb.append(br.readLine());
+		sb.append("??!");
+		
+		System.out.println(sb.toString());
+	}
+}
